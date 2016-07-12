1f7b6b45956c1ce4936afe0550915fd3e347f629
pandabrand <pandabrand@gmail.com>
2016-07-12 17:49:26 -0500

    Added element fot hero image and text and styling.

--- imports/ui/home.jsx
+++ imports/ui/home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import SelectBar from './select-bar.jsx';
+
 export const Home = () => (
-  <div className="container" id="main">
-    <img src="http://lorempixel.com/image_output/nature-q-g-1210-480-6.jpg" />
-    <div className="heroText">
-      This is where text will go.
+  <div id="main">
+    <img className="hero-image" src="http://lorempixel.com/g/1210/480/nature/1/Dummy-Text" />
+    <div className="hero-text">
+      <div className="hero-title">This is where text will go.</div>
+      <div className="hero-copy">Now we will have more text below.</div>
     </div>
+    <SelectBar/>
   </div>
 );
---
+++ imports/ui/select-bar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SelectBar extends Component {
+  render(){
+   return (
+     <div className="container select-bar">
+      Pick from one of our featured guides below or use the search bar to filter by artist, city and category
+     </div>
+   );
+  }
+ }
