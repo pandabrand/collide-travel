ddbac74f7022f247bceb9e3df0a8941ec0dd6422
pandabrand <pandabrand@gmail.com>
2016-07-14 13:59:23 -0500

    print page

--- imports/ui/data/magazine.jsx
+++ imports/ui/data/magazine.jsx
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 
 export default class Magazine extends Component {
   render() {
-    const soldout = this.props.print.purchase && this.props.print.download;
+    const soldout = !this.props.print.purchase && !this.props.print.download;
     return (
       <div className="print-issue col-sm-6 col-md-4">
         <div className="print-preview-image thumbnail">
@@ -10,13 +10,13 @@ export default class Magazine extends Component {
         </div>
         <div className="print-control caption">
           <p>{this.props.print.featured}</p>
-          { this.soldout ?
+          { !this.props.print.purchase && !this.props.print.download ?
             <div className="soldout">
             <h3 className="soldout">Sold Out</h3>
             <p className="cta">I still want it!</p></div> :
             <div className="purchse-download">
-            {this.props.print.purchase ? <button className="print-control-button buy">Buy Now</button> : ''}
-            {this.props.print.download ? <button className="print-control-button download">Download</button> : ''}
+            {this.props.print.purchase ? <button className="btn btn-primary print-control-button buy">Buy Now</button> : ''}
+            {this.props.print.download ? <button className="btn btn-primary print-control-button download">Download</button> : ''}
             </div>
           }
         </div>
