8c71c4f16b0964cb918452954016a322506f5f85
pandabrand <pandabrand@gmail.com>
2016-07-22 14:45:21 -0500

    geolocation added and default spinner added, needs styling

--- imports/ui/google-maps.jsx
+++ imports/ui/google-maps.jsx
@@ -5,10 +5,14 @@ import GoogleMap from 'google-map-react';
 import MapMarker from './mapping/map-marker.jsx';
 import MapTable from './mapping/map-table.jsx';
 
+import Spinner from './app/spinner.jsx'
+import Loading from 'react-loading-spinner';
+import 'react-loading-spinner/src/css/index.css';
+
 const MAP_KEY = Meteor.settings.public.GMAP_KEY;
+let pos = {};
 
 export default class HomeMap extends Component {
-
   static propTypes = {
     center: PropTypes.array,
     zoom: PropTypes.number,
@@ -16,7 +20,7 @@ export default class HomeMap extends Component {
   };
 
   static defaultProps = {
-    center: [41.928432, -87.707342],
+    center: [34.052234, -118.243685],
     zoom: 12,
     greatPlaces: [
      {id: 'A', lat: 41.927665, lng: -87.706945},
@@ -28,25 +32,40 @@ export default class HomeMap extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {pos: null};
+  }
+
+  componentWillMount() {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {lat: position.coords.latitude, lng: position.coords.longitude};
+        this.setState({pos: {lat: position.coords.latitude, lng: position.coords.longitude}});
+      }.bind(this));
+    }
   }
 
   render() {
-
-    return (
-      <div className="map-container">
-        <div className="table-container">
-          <MapTable/>
-        </div>
-        <div className="map-layout">
-          <GoogleMap
-            bootstrapURLKeys={{key: MAP_KEY}}
-            center={this.props.center}
-            zoom={this.props.zoom}>
-              <MapMarker lat={41.927665} lng={-87.706945} text={'A'} />
-              <MapMarker lat={41.923945} lng={-87.699295} text={'B'} />
-          </GoogleMap>
-        </div>
+    let mapContent;
+    if(this.state.pos !== null) {
+      mapContent = <div className="map-container">
+      <div className="table-container">
+        <MapTable/>
       </div>
+      <div className="map-layout">
+        <GoogleMap
+          bootstrapURLKeys={{key: MAP_KEY}}
+          center={Object.keys(this.state.pos).length !== 0 ? this.state.pos : this.props.center}
+          zoom={this.props.zoom}>
+            <MapMarker lat={41.927665} lng={-87.706945} text={'A'} />
+            <MapMarker lat={41.923945} lng={-87.699295} text={'B'} />
+        </GoogleMap>
+      </div>
+      </div>;
+    } else {
+      mapContent = <Loading isLoading={this.state.pos === null} loadingClassName='loading' />;
+    }
+    return (
+      <div>{mapContent}</div>
     );
   }
 }
