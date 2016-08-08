3da8286612533071b17aabf98216d8dce703581f
pandabrand <pandabrand@gmail.com>
2016-08-07 22:02:45 -0500

    added pluralize library used in dashboard.

--- client/components/admin/overview/info-box.jsx
+++ client/components/admin/overview/info-box.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import pluralize from 'pluralize';
 
 const getCollectionInfoBox = (info) => {
   return <div className="collection-info-box">
       <div className="collection-info-header">{info.name}</div>
       <div className="collection-info-details">
         {info.count}
-        <span>{info.name}s</span>
+        <span>{pluralize(info.name, info.count)}</span>
       </div>
       <div className="collection-info-link"><a href={FlowRouter.path(info.editRoute)}>View more <i className="fa fa-info-circle"/></a></div>
     </div>;
