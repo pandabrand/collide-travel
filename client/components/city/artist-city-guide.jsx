bf400cbfff9b8101257f2b6a8a7e0e1a2c0a881b
pandabrand <pandabrand@gmail.com>
2016-08-08 12:16:28 -0500

    photo tweaks

--- client/components/city/artist-city-guide.jsx
+++ client/components/city/artist-city-guide.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ArtistCityGuideComponent({artist, city}) {
   return (
     <div className="row artist-row">
-      <div className="col-sm-3 col-md-4 image side"><img src="http://lorempixel.com/320/320/people/2"/></div>
+      <div className="col-sm-3 col-md-4 image side"><div className="side-image"><img src="http://lorempixel.com/320/320/people/2"/></div></div>
       <div className="col-sm-9 col-md-8 content main">
         <h1 className="content-header">{artist.artistName}</h1>
         <h4>{city.displayName}</h4>
--- client/components/city/city-guide.jsx
+++ client/components/city/city-guide.jsx
@@ -4,7 +4,7 @@ export default function CityGuideComponent({city}) {
   const createMarkup = () => { return {__html: city.description}; };
   return (
     <div className="row city-row">
-      <div className="col-sm-3 col-md-4 image side"><img src="http://lorempixel.com/320/320/city/2"/></div>
+      <div className="col-sm-3 col-md-4 image side"><div className="side-image"><img src="http://lorempixel.com/320/320/city/2"/></div></div>
       <div className="col-sm-9 col-md-8 content main">
         <h1 className="content-header">{city.displayName}</h1>
         <div className="city-copy">
