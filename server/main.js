88015e4b766dfea4f349d945cf7fe983e6f014f5
pandabrand <pandabrand@gmail.com>
2016-08-16 16:16:45 -0500

    moving cloudinary config futher down

--- server/main.js
+++ server/main.js
@@ -3,12 +3,12 @@ import { Meteor } from 'meteor/meteor';
 import { CitiesCollection } from '../lib/collections/cities.js';
 const MAP_KEY = Meteor.settings.public.GMAP_KEY;
 
-Cloudinary.config ({
-	cloud_name:Meteor.settings.public.CLOUDINARY_CLOUD_NAME,
-  api_key:Meteor.settings.public.CLOUDINARY_API_KEY,
-  api_secret:Meteor.settings.CLOUDINARY_API_SECRET,
-});
 
 Meteor.startup(() => {
+	Cloudinary.config ({
+		cloud_name:Meteor.settings.public.CLOUDINARY_CLOUD_NAME,
+	  api_key:Meteor.settings.public.CLOUDINARY_API_KEY,
+	  api_secret:Meteor.settings.CLOUDINARY_API_SECRET,
+	});
   // code to run on server at startup
 });
