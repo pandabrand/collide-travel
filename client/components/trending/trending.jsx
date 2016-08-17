7ffe50ed2628d9548b88a877aa38b04588690a9c
pandabrand <pandabrand@gmail.com>
2016-08-16 20:37:15 -0500

    trending styling in place

--- client/components/trending/trending.jsx
+++ client/components/trending/trending.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
+const createMarkup = (markup) => { return {__html: markup}; };
 
-  export default TrendingComponent = ({trendingArticles}) => 
+  export default TrendingComponent = ({trendingArticles}) =>
   (<div className="container-fluid featured-container">
           <div className="featured-row row">
             <h1 className="featured-header">TRENDING</h1>
@@ -9,7 +10,21 @@ import React, { Component, PropTypes } from 'react';
             <div className="trending-gallery">
             {trendingArticles.map((article, i) => {
               return <div key={i} className="col-md-4 col-sm-6">
-                <img src="http://lorempixel.com/375/550/fashion" />
+                <a href={article.link} target="_blank">
+                <div className="trending-container">
+                  <div className="trending-image">
+                    <img className="img-responsive" src={article.image} />
+                  </div>
+                  <div className="trending-article">
+                    <div className="trending-header">
+                      <div dangerouslySetInnerHTML={createMarkup(article.secondaryTitle)}/>
+                    </div>
+                    <div className="trending-body">
+                      <div dangerouslySetInnerHTML={createMarkup(article.secondaryDescription)}/>
+                    </div>
+                  </div>
+                </div>
+                </a>
               </div>;
             })}
             </div>
