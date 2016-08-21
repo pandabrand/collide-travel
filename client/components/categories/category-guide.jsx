921145cc145a2efed85a3c1c9036f0d0d98bdb43
pandabrand <pandabrand@gmail.com>
2016-08-21 15:17:09 -0500

    clean up on aisle category

--- client/components/categories/category-guide.jsx
+++ client/components/categories/category-guide.jsx
@@ -4,8 +4,8 @@ import pluralize from 'pluralize';
 export default function CategoryGuideComponent({category}) {
   return (
     <div className="content-header row">
-      <div className="col-md-8">
-      <h1>{pluralize(category, 2)}</h1>
+      <div className="col-md-8 category-header">
+        <h1>{pluralize(category, 2)}</h1>
       </div>
     </div>
   );
