0aec78c4f817a178dac7714cf49865b6cd6f6409
pandabrand <pandabrand@gmail.com>
2016-09-08 14:25:45 -0500

    added border to city guides

--- client/components/print-page/print-element.jsx
+++ client/components/print-page/print-element.jsx
@@ -4,7 +4,7 @@ import {createMarkup, cloudinaryURL} from '../../lib/utils.js';
 export default PrintElementComponent = ({print}) => {
   const soldout = !print.purchase && !print.download;
   return (
-    <div className="row print-row print-issue">
+    <div className="row print-row trending-row print-issue">
     <div className="col-xs-12 col-sm-6 col-md-4">
         <div className="print-preview-image">
           <img className="img-responsive" src={cloudinaryURL(print.printPreview, 280, 390, 'fit')}/>
