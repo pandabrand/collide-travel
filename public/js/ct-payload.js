ccfbc2aef4a98e3bd20cb2faa704e57bc42f8f57
pandabrand <pandabrand@gmail.com>
2016-10-04 15:55:49 -0500

    changes to ct-payload

--- public/js/ct-payload.js
+++ public/js/ct-payload.js
@@ -1,9 +1,12 @@
 (function(global) {
   function attachAndCreate() {
+    console.dir('called');
     let cc_info = document.getElementById('cc-info');
     if(!cc_info) {
+      console.dir(cc_info);
       return;
     } else {
+      console.dir('building...');
       let div_doc = document.createElement('div');
       let text_doc = document.createTextNode('Hi There.');
       div_doc.appendChild(text_doc);
