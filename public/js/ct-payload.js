7d97743a8178b8509c30aec1ab113d857c859126
pandabrand <pandabrand@gmail.com>
2016-10-04 17:09:38 -0500

    changes to ct-payload

--- public/js/ct-payload.js
+++ public/js/ct-payload.js
@@ -1,16 +1,18 @@
 (function(global) {
-  console.dir('hi there.');
   attachAndCreate();
 
 
   function attachAndCreate() {
-    console.dir('called');
     let cc_info = document.getElementById('cc-info');
     if(!cc_info) {
-      console.dir(cc_info);
       return;
     } else {
-      console.dir('building...');
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", "https://collide-travel.herokuapp.com/api/cities/chicago", false);
+      xhr.send();
+
+      console.dir(xhr.status);
+      console.dir(xhr.statusText);
       let div_doc = document.createElement('div');
       let text_doc = document.createTextNode('Hi There.');
       div_doc.appendChild(text_doc);
