a6b39c16a4af4f382ed993085642ab062104c88b
pandabrand <pandabrand@gmail.com>
2016-10-04 15:09:40 -0500

    changes to ct-payload

--- public/js/ct-payload.js
+++ public/js/ct-payload.js
@@ -1,1 +1,11 @@
-// hi there
+(function(global) {
+  let cc_info = document.getElementById('cc-info');
+  if(!cc_info) {
+    return;
+  } else {
+    let div_doc = document.createElement('div');
+    let text_doc = document.createTextNode('Hi There.');
+    div_doc.appendChild(text_doc);
+    cc_info.appendChild.div_doc;
+  }
+})();// hi there
