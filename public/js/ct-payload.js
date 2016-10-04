6c38034bb999bccb7279142ebeecf51b39551412
pandabrand <pandabrand@gmail.com>
2016-10-04 16:31:38 -0500

    changes to ct-payload

--- public/js/ct-payload.js
+++ public/js/ct-payload.js
@@ -2,7 +2,7 @@
   console.dir('hi there.');
   attachAndCreate();
 
-  
+
   function attachAndCreate() {
     console.dir('called');
     let cc_info = document.getElementById('cc-info');
@@ -14,7 +14,7 @@
       let div_doc = document.createElement('div');
       let text_doc = document.createTextNode('Hi There.');
       div_doc.appendChild(text_doc);
-      cc_info.appendChild.div_doc;
+      cc_info.appendChild(div_doc);
     }
   }
 })();// hi there
