7272845504d9b3cd85d3702b19bf33135ad250a2
pandabrand <pandabrand@gmail.com>
2016-10-04 15:47:58 -0500

    changes to ct-payload

--- public/js/ct-payload.js
+++ public/js/ct-payload.js
@@ -1,11 +1,18 @@
 (function(global) {
-  let cc_info = document.getElementById('cc-info');
-  if(!cc_info) {
-    return;
-  } else {
-    let div_doc = document.createElement('div');
-    let text_doc = document.createTextNode('Hi There.');
-    div_doc.appendChild(text_doc);
-    cc_info.appendChild.div_doc;
+  function attachAndCreate() {
+    let cc_info = document.getElementById('cc-info');
+    if(!cc_info) {
+      return;
+    } else {
+      let div_doc = document.createElement('div');
+      let text_doc = document.createTextNode('Hi There.');
+      div_doc.appendChild(text_doc);
+      cc_info.appendChild.div_doc;
+    }
   }
+
+  if (window.attachEvent)
+      window.attachEvent('onload', attachAndCreate);
+  else
+      window.addEventListener('load', attachAndCreate, false);
 })();// hi there
