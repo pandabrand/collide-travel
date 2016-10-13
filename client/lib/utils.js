c6d19b490d02ddfce5001543f8cfde1b28c25fa1
pandabrand <pandabrand@gmail.com>
2016-10-13 15:40:06 -0500

    added prerender io

--- client/lib/utils.js
+++ client/lib/utils.js
@@ -7,6 +7,9 @@ const createMarkup = (markup) => {
 };
 
 const cloudinaryURL = (imgStr, w, h, crop="fill", gravity="auto") => {
+  if(!imgStr) {
+    return;
+  }
   const imgFile = imgStr.lastIndexOf('/') === -1 ? imgStr : imgStr.substr(imgStr.lastIndexOf('/') + 1);
   let cloudObj = (crop === "fill" || crop === "thumb") ? {width:w, gravity: gravity, crop:crop, dpr: window.devicePixelRatio} : {width:w, crop:crop, dpr: window.devicePixelRatio};
   if(h && h > 0) {
