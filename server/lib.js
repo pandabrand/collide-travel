a99ce9b1abb81ead5ec4eeec11c10645bd46b3f5
pandabrand <pandabrand@gmail.com>
2016-11-03 19:45:12 -0500

    adding defer script loading for SSR

--- server/lib.js
+++ server/lib.js
@@ -1,3 +1,3 @@
 Meteor.startup(() => {
-  process.env.CLOUDINARY_URL = 'cloudinary://892538286235217:XpW6kP2EGG1qFMW66Y7cPx-lhWk@cc-travel'
+  // process.env.CLOUDINARY_URL = 'cloudinary://892538286235217:XpW6kP2EGG1qFMW66Y7cPx-lhWk@cc-travel'
 });
